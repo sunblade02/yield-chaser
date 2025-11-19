5af9b754f2eba140df5bffe0e992bbc2e15910a8
sunblade02 <riquier.jeremie@gmail.com>
2025-11-19 23:22:48 +0000

    Creating home page

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter  } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/provider/ThemeProvider";
+import Layout from "@/components/shared/Layout";
 
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
+const inter = Inter({
+    variable: '--font-inter',
+    subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
@@ -26,7 +22,7 @@ export default function RootLayout({
     return (
         <html lang="en" suppressHydrationWarning>
             <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+                className={`${inter.variable} antialiased`}
             >
                 <ThemeProvider
                     attribute="class"
@@ -34,7 +30,7 @@ export default function RootLayout({
                     enableSystem
                     disableTransitionOnChange
                 >
-                    {children}
+                    <Layout>{children}</Layout>
                 </ThemeProvider>
             </body>
         </html>
---
+++ frontend/src/components/shared/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="pb-12 text-center text-muted-foreground text-sm">
+            Yield Chaser — Your gateway to safer DeFi yields.
+        </footer>
+    )
+}
+
+export default Footer
