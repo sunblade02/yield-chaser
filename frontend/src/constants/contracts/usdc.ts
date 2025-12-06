d0974c4ee191da4666983fd6b09ead7ff8b9164a
sunblade02 <riquier.jeremie@gmail.com>
2025-12-06 17:45:08 +0100

    Fix staging

--- frontend/src/constants/contracts/usdc.ts
+++ frontend/src/constants/contracts/usdc.ts
@@ -1,6 +1,6 @@
 const ENV = process.env.NEXT_PUBLIC_ENV || "dev";
 
-let address = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // hardhat;
+let address: `0x${string}` = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // hardhat;
 switch (ENV) {
   case "staging": // sepolia
     address = "0x361680F6052786187dFEe22355eD18113A8de3DC";
